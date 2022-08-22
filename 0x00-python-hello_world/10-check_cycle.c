bc82e07ddb802153c96f454c6d52c02cf1908b41
befman123 <befman123@gmail.com>
2022-08-22 22:30:31 +0300

    challenge #10 DONE!

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *check_cycle - checks if there is  cycle ina linked list
+ *
+ *@list: the linked list to check
+ *Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	temp = list;
+	while (temp = temp->next)
+		if (temp == list)
+			return (1);
+	return (0);
+}
