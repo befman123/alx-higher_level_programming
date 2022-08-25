0ab5fdc14d09b5f031fe52a615e68cf2d4fc5cd1
befman123 <befman123@gmail.com>
2022-08-25 22:53:15 +0300

    Challnge #2 DONE!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ import sys as s
 
 
 def my_function():
+    if !len(s.argv):
+        print("0 arguments.")
     print("{0} arguments:".format(len(s.argv)))
     for i in range(1, len(s.argv)):
         print("{0}: {1}".format(i, s.argv[i]))
