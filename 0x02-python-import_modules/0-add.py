0b43e1abfd27a917b3113d36af15e8cea0292747
befman123 <befman123@gmail.com>
2022-08-25 21:51:21 +0300

    Challenge #0 DONE!

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,9 +5,8 @@ import add_0
 def my_add():
     a = 1
     b = 2
-    print(f"{a} + {b} = {add_0.add(a,b)}")
+    print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
 
 
 if (__name__ == "__main__"):
     my_add()
-
