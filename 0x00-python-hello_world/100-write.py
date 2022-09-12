b8c1671ab2436ab83a85f2d4feed7c17e3ba2661
befman123 <befman123@gmail.com>
2022-09-12 14:29:14 +0300

    Challnege #11 DONE!

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+from sys import exit
+
+
+def start():
+    stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+    exit(1)
+
+
+if __name__ == "__main__":
+    start()
