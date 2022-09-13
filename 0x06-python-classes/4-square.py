ba1bea3ec29642db0079dadb834767ee9aaf33d0
befman123 <befman123@gmail.com>
2022-09-13 20:57:25 +0300

    Challenge #4 DONE!

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A module with a class and attribute"""
+
+
+class Square:
+    """A class square with size private attribute
+
+    Attributes:
+        size (int): the size of one side of a square
+    """
+    __size = 0
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be {0} 0".format(">="))
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of a square
+        Returns:
+            the area of the square
+        """
+        return self.__size ** 2
+
+    def size(self, value):
+        """A setter for the private size attribute
+        Args:
+            value: the value to set to size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be {0} 0".format(">="))
+        else:
+            self.__size = value
+
+
+if __name__ == "__main__":
+    a = Square()
