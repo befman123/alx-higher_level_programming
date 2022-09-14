bcb3903410a831887d23b13e369b858f3fedea7e
befman123 <befman123@gmail.com>
2022-09-14 20:49:51 +0300

    Challenge #8 DONE!

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""A module with a class and attribute"""
+
+
+class Square:
+    """A class square with size private attribute
+
+    Attributes:
+        size (int): the size of one side of a square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be {0} 0".format(">="))
+        else:
+            self._size = size
+
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) is not int or position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[1]) is not int or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self._position = position
+
+    def area(self):
+        """Calculates the area of a square
+        Returns:
+            the area of the square
+        """
+        return self._size ** 2
+
+    def my_print(self):
+        if self.size != 0:
+            for x in range(0, self.position[1]):
+                print()
+            for i in range(0, self.size):
+                for w in range(0, self.position[0]):
+                    print("{0}".format(" "), end="")
+                for j in range(0, self.size):
+                    print("{0}".format("#"), end="")
+                print("")
+        else:
+            print("")
+
+    def size(self, value=None):
+        """A setter for the private size attribute
+        Args:
+            value: the value to set to size
+        """
+        if value is None:
+            return self._size
+        elif type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be {0} 0".format(">="))
+        else:
+            self._size = value
+
+    def position(self, value=None):
+        """A setter for the private size position
+        Args:
+            value: the value to set to position
+        """
+        error_string = "position must be a tuple of 2 positive integers"
+        if value is None:
+            return self._position
+        elif len(value) != 2:
+            raise TypeError(error_string)
+        elif type(value) is not tuple:
+            raise TypeError(error_string)
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError(error_string)
+        elif type(value[1]) is not int or value[1] < 0:
+            raise TypeError(error_string)
+        else:
+            self._position = value
+
+    
+    def __str__(self):
+            s = ""
+            if self.size != 0:
+                for x in range(0, self.position[1]):
+                    s = s + "\n"
+                for i in range(0, self.size):
+                    for w in range(0, self.position[0]):
+                        s = s + " "
+                    for j in range(0, self.size):
+                        s = s + "#"
+                    s = s + "\n"
+            else:
+                s = s + "\n"
+            return s
+    
+    size = property(size, size)
+    position = property(position, position)
+
+
+if __name__ == "__main__":
+    b = Square(3, (3, 1))
+    b.my_print()
+    print(b)
