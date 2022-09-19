eac73dd208e0e72cf4ba16c8ada86effba8c8549
befman123 <befman123@gmail.com>
2022-09-19 18:44:02 +0300

    Challene #1 DONE!

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,9 +6,9 @@ class Rectangle:
     """ The class Rectangle
 
     Args:
-    width: private positive integer
-    height: private postive integer
-"""
+    __width: private positive integer
+    __height: private postive integer
+    """
 
     def __init__(self, width=0, height=0):
 
@@ -19,12 +19,8 @@ class Rectangle:
         height: a positive integer used as a width of a rectangle
 
         """
-        self.height(height)
-        self.width(width)
-
-    def height(self, height=None):
         if height is None:
-            return __height
+            return self.__height
         elif type(height) is not int:
             raise TypeError("height must be an integer")
         elif height < 0:
@@ -32,7 +28,6 @@ class Rectangle:
         else:
             self.__height = height
 
-    def width(self, width=None):
         if width is None:
             return self.__width
         elif type(width) is not int:
@@ -42,6 +37,31 @@ class Rectangle:
         else:
             self.__width = width
 
+    def height(self, value=None):
+        if value is None:
+            return self.__height
+        elif type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be {0} 0".format(">="))
+        else:
+            self.__height = value
+
+    def width(self, value=None):
+        if value is None:
+            return self.__width
+        elif type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be {0} 0".format(">="))
+        else:
+            self.__width = value
+
+    height = property(height, height)
+    width = property(width, width)
+
 
 if __name__ == "__main__":
-    a = Rectangle()
+    a = Rectangle(1, 12)
+    print(a.width)
+    print(a.height)
