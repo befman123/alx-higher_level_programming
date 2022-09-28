6ea55ba2ffa2d13367062acf6d67ab8e19251a01
befman123 <befman123@gmail.com>
2022-09-28 21:41:34 +0300

    Challenge #5 DONE!

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,15 +7,13 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation
+
     Arguments:
             my_obj: any serializable object
             filename: the file to write the json of my_obj
     """
-    try:
-        with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
             json.dump(my_obj, f)
-    except PermissionError:
-        raise PermissionError("")
 
 
 if __name__ == "__main__":
