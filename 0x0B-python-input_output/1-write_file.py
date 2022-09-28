2262443131202d189e1a48e0a4e09a8b0fbd62c3
befman123 <befman123@gmail.com>
2022-09-28 16:07:54 +0300

    Challenge #1 DONE!

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Writes to a new file"""
+
+
+def write_file(filename="", text=""):
+    """This function does the actual work
+
+    Arguments:
+        filename: a string representing the file name
+        text: the string to be written to the file
+    """
+    if filename == "" or filename is None or type(filename) is not str:
+        pass
+    else:
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(text+"\n")
+
+
+if __name__ == "__main__":
+    pass
