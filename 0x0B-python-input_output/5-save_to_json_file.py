d05a10cec7174b05459695a62fa21cb38fa7658c
befman123 <befman123@gmail.com>
2022-09-28 20:40:44 +0300

    Challenge #5 DONE!

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""writes an Object to a text file, using a JSON representation"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation
+    Arguments:
+            my_obj: any serializable object
+            filename: the file to write the json of my_obj
+    """
+    try:
+        with open(filename, "a", encoding="utf-8") as f:
+            json.dump(my_obj, f)
+    except PermissionError:
+        pass
+
+
+if __name__ == "__main__":
+    pass
