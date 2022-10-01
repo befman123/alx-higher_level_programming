317159013d693ff8c6cf6acdc0c491976f104cb3
befman123 <befman123@gmail.com>
2022-10-01 13:00:29 +0300

    Challenge #9 finally DONE!

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Prints the last digit of a number"""
+
+
+def print_last_digit(number):
+    number = int(number)
+    print("{0}".format(number.__abs__() % 10))
+
+
+if __name__ == "__main__":
+    print_last_digit(-1204)
