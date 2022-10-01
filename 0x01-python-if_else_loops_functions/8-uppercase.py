e1797bb61342bff311778c91076f94469cc15916
befman123 <befman123@gmail.com>
2022-10-01 11:41:43 +0300

    Challenge #8 finally DONE!

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Prints the given string in all caps"""
+
+
+def uppercase(str):
+    upper_str = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            upper_str += chr(ord(i) - 32)
+        else:
+            upper_str += i
+    return upper_str
+
+
+if __name__ == "__main__":
+    print(uppercase("befiker damtew"))
