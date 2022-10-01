432f9b76f0112c49a092de1305ec15c86dc54314
befman123 <befman123@gmail.com>
2022-10-01 13:32:58 +0300

    Challenge #12 finally DONE!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,7 +8,9 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("{0}".format("FizzBuzz"), end=" ")
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
