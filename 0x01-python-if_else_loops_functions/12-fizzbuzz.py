9f7a5565ae38fa518f93f77ea1d3c2a15cddc243
befman123 <befman123@gmail.com>
2022-10-01 13:29:16 +0300

    Challenge #12 finally DONE!

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints numbers from 1 - 100
+   if divisible by 3 print fizz
+   if divisible by 5 print buzz
+   if both print fizzbuzz
+"""
+
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{0}".format(i), end=" ")
+
+
+if __name__ == "__main__":
+    fizzbuzz()
