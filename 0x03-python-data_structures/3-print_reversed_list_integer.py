7653888ddec25a45f3ed452681b93fdf6055564a
befman123 <befman123@gmail.com>
2022-10-02 14:49:58 +0300

    Challenge #3 DONE!

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module has a function that prints
+    the elements of an integer list in reverse
+"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    for i in range(0, len(my_list)).__reversed__():
+        print("{:d}".format(my_list[i]))
+
+
+if __name__ == "__main__":
+    print_list_integer([1, 2, 3, 4, 5])
