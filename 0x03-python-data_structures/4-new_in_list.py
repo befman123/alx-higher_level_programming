d0f8711b1a4587b6a9044a4258b0df68e3873a11
befman123 <befman123@gmail.com>
2022-10-02 15:58:32 +0300

    Challenge #4 finally DONE!

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Modifies a copy of a list"""
+
+
+def new_in_list(my_list, idx, element):
+    ret = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return ret
+    else:
+        ret[idx] = element
+
+
+if __name__ == "__main__":
+    pass
