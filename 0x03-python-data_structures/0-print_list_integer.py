a21cb952721a21034df5ae57f9c8b6e7e0ab8d28
befman123 <befman123@gmail.com>
2022-10-02 13:21:43 +0300

    Challenge #0 finally DONE!

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,7 +6,7 @@
 
 def print_list_integer(my_list=[]):
     for i in range(0, len(my_list)):
-        print('{:n}'.format(my_list[i]))
+        print('{0:d}'.format(my_list[i]))
 
 
 if __name__ == "__main__":
