c1fdffd24dbc69ad6c3de35b6161ea75fd76c7c4
befman123 <befman123@gmail.com>
2022-10-03 17:29:54 +0300

    Finished Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,6 +77,59 @@ class Rectangle(base.Base):
         else:
             self.__y = value
 
+    def area(self):
+        """ Calculates area as width * height
+
+        Returns:
+            the area of the rectangle
+        """
+        return self.height * self.width
+
+    def display(self):
+        """ print in stdout the Rectangle instance with
+            the character # by taking care of x and y
+        """
+        for h in range(0, self.height):
+            if self.y > 0:
+                for xs in range(0, self.x):
+                    print(" ", end="")
+            for w in range(0, self.width):
+                print("".format("#"), end="")
+            print()
+
+    def __str__(self):
+        """ Returns string representation as the required
+            format"""
+
+        s = f"[Rectangle] ({self.id}) {self.x}/{self.y} -"
+        s += f" {self.width}/{self.height}"
+        return s
+
+    def update(self, *args, **kwargs):
+        """ updates self with *args values
+
+        Arguments:
+            *args:  a built in list for acceptting
+                    variable number arguments in a
+                    function
+            **kwargs: need i explain?
+        Note:
+            Argument order is super important, and
+            number of args should be five
+        """
+        if len(args) == 0:
+            self.width = kwargs["width"]
+            self.height = kwargs["height"]
+            self.x = kwargs["x"]
+            self.y = kwargs["y"]
+            self.id = kwargs["id"]
+        else:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+
 
 if __name__ == "__main__":
     pass
