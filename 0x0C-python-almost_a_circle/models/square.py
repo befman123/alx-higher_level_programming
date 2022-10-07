b9fd2e13c3089b34f023b8c4bf2a3f29af66046e
befman123 <befman123@gmail.com>
2022-10-07 17:32:54 +0300

    Commented the whole module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,22 +16,61 @@ class Square(rectangle.Rectangle):
             and height are the same
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """ Object creation method for class square
+            Arguments:
+                size: positive integer that is assigned to both
+                      height and width
+                x: assigned to inherited property x
+                y: assigned to inherited property y
+                id: assigned to inherited instance variable 
+                    (from base.Base) id
+            Note:
+                Only super is called since a square has both height
+                and width it's just that they are equal
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ The string representation of this class"""
         s = f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
         return s
 
     @property
     def size(self):
-        return self.width
+        """ getter method for the property size
+            Returns:
+                self.width since both height and width are equal
+                (if it exists) or self.height(if self.width is None)
+        """
+        if self.width is not None:
+            return self.width
+        else:
+            return self.height
 
     @size.setter
     def size(self, value):
+        """ setter method for the property size
+            Arguments:
+                value: int value set to both width and 
+                        height accordingly
+            
+            Note:
+                assigning value to rectangles height and width
+                will pass through the setters in the parent class
+                for the corresponding height and width properties
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """ Updates the object using *args or **kwargs.
+            if *args is empty it tries to use **kwargs
+
+            Note:
+                The order of assignment if *args is used is 
+                important ["id", "size", "x", "y"] 
+        
+        """
         list_args = ["id", "size", "x", "y"]
 
         if len(args) != 0:
