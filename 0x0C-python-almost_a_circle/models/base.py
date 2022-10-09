10057f5cdffb3ef4c15eb3942381e78d2bb32e0c
befman123 <befman123@gmail.com>
2022-10-09 15:19:09 +0300

    Added a load from and save to file functions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -110,7 +110,7 @@ class Base:
                 list_dict = cls.from_json_string(f.read())
                 for dict in list_dict:
                     obj = cls(1, 1)
-                    obj.create(**dict)
+                    obj = obj.create(**dict)
                     list_inst.append(obj) 
         return list_inst
 
