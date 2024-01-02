69c9cb34aafede60fee27f9b4ecc26a7ca993fb9
befman123 <befman123@gmail.com>
2024-01-02 17:18:14 +0300

    Challenge #7 DONE!

---
+++ 0x00-python-hello_world/6-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+str1 = f"""First 3 letters: {str1[0:3]}
+           Last 2 letters: {str1[-2]}
+           Middle word: {str1[1:-1]}"""
+print(f"Welcome to {str1}!")
