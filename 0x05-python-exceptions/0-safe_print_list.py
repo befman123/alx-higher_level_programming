2a52b665949211ebb6b066008397144ad8cfc4bb
befman123 <befman123@gmail.com>
2024-01-22 22:11:38 +0300

    Challenge #1 DONE!

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[a], end="")
         except IndexError:
             break
-    if x != 0:
-        print()
+    print()
     return (a)
