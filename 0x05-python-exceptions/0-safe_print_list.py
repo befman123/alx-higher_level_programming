c0c169b2e409c1e7a3a55ec3fe72905406642a6b
befman123 <befman123@gmail.com>
2024-01-22 21:53:34 +0300

    Challenge #1 DONE!

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
+    a = 0
+    for a in range(0, x):
         try:
-            print(my_list[i], end="")
+            print(my_list[a], end="")
         except IndexError:
             print("Error", end="")
     print()
-ls = ["tefera", 2, 3]
-safe_print_list(ls, 4)
+    return (a)
