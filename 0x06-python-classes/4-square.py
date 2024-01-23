4ef99160f0d3e2b62f61a57fb5c2f9f79e7d44e4
befman123 <befman123@gmail.com>
2024-01-23 15:27:46 +0300

    Added a property called size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module 0-square.py that defines a Square class"""
+
+
+class Square:
+    """ class Square"""
+    def __init__(self, size=0):
+        """ We define the size variable as a private instance of the class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of a square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns the value private instance variable size"""
+        return self.__size
+
+
+if __name__ == "__main__":
+    pass
