1739929cf3a12850d30da569e73e9199e683dd05
befman123 <befman123@gmail.com>
2024-01-23 15:10:46 +0300

    Added a private instance variable size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module 0-square.py that defines a Square class"""
+
+
+class Square:
+    """ class Square"""
+    def __init__(size):
+        """ We define the size variable as a private instance of the class"""
+        self.__size = size
+
+
+if __name__ == "__main__":
+    pass
