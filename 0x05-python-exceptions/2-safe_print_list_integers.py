c2d87706ef9d3b9c246eb5b897448f78c1ae13da
befman123 <befman123@gmail.com>
2024-01-24 13:20:32 +0300

    Challenge #2 DONE!

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             a += 1
         except TypeError:
             a += 1
+        except ValueError:
+            a += 1
     print()
     return (a)
